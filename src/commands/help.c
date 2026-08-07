88502096f7d617bae969e3c88a183a45aeb4a86a
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-08-07 17:17:20 +0900

    helpコマンドをいい感じにする

--- src/commands/help.c
+++ src/commands/help.c
@@ -14,7 +14,18 @@ void print_help(const int argc, char *argv[]) {
 	(void)argc;
 	(void)argv;
 
-	printf(BLUE "usage: " RESET MAGENTA "craft" RESET " [" GREEN "-h" RESET
-		    "," GREEN "help" RESET "]\n");
+	printf(BLUE "Usage:\n" RESET "\t" MAGENTA "craft" RESET
+		    " <command> [arguments]\n"
+		    "\n" BLUE "Commands:\n" RESET "\t" GREEN "build" RESET
+		    "\t[target]\tBuild the project or a target\n"
+		    "\t" GREEN "run" RESET
+		    "\t[target]\tBuild and run an executable target\n"
+		    "\t" GREEN "clean" RESET "\t\t\tRemove build artifacts\n"
+		    "\t" GREEN "version" RESET "\t\t\tPrint Craft version\n"
+		    "\t" GREEN "help" RESET "\t\t\tPrint this help\n"
+		    "\n" BLUE "Options:\n" RESET "\t" GREEN "-h" RESET
+		    "\t\t\tPrint this help\n"
+		    "\t" GREEN "-v" RESET "\t\t\tPrint Craft version\n");
+
 	fflush(stdout);
 }
