df92e7d1a6a6cc99242f8606228cd2143168da91
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-08-07 11:00:57 +0900

    アプリの情報を定義するapp.hを作成

---
+++ src/app.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2026 tas0dev
+ * This software is licensed under the GNU General Public License, version 3
+ * only.
+ *
+ * Created by tas0dev
+ */
+
+#ifndef CRAFT_APP_H
+#define CRAFT_APP_H
+
+#define VERSION "0.1"
+#define AUTHOR "tas0dev"
+#define BUILD __DATE__
+
+#endif // CRAFT_APP_H
