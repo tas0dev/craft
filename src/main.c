a90ecf6ecabfa6785e6682e1d8af3eb80d5839ab
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-08-07 10:52:42 +0900

    コピーライト表記

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2026 tas0dev
+ * This software is licensed under the GNU General Public License, version 3
+ * only.
+ *
+ * Created by tas0dev
+ */
+
 #include <stdio.h>
 
 int main(void) {
