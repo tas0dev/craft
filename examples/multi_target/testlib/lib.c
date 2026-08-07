14035dbd8451a590203e37cf60dcc0b4b76f5caf
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-08-07 17:02:08 +0900

    複数ターゲットのexampleを作成

---
+++ examples/multi_target/testlib/lib.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2026 tas0dev
+ * This software is licensed under the GNU General Public License, version 3
+ * only.
+ *
+ * Created by tas0dev
+ */
+
+#include "lib.h"
+#include <stdio.h>
+
+int print_hello(void) {
+	printf("Hello World!\n");
+	return 0;
+}
---
+++ examples/multi_target/testlib/lib.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2026 tas0dev
+ * This software is licensed under the GNU General Public License, version 3
+ * only.
+ *
+ * Created by tas0dev
+ */
+
+#ifndef CRAFT_LIBC_H
+#define CRAFT_LIBC_H
+
+int print_hello(void);
+
+#endif // CRAFT_LIBC_H
