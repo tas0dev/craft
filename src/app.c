76ccdb283337d3f07b1aff9c71512a3ec028f629
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-08-08 11:18:03 +0900

    --help, --versionも使えるように

--- src/app.c
+++ src/app.c
@@ -10,9 +10,11 @@
 #include <string.h>
 
 enum Commands match_commands(const char *command) {
-	if (strcmp(command, "help") == 0 || strcmp(command, "-h") == 0)
+	if (strcmp(command, "help") == 0 || strcmp(command, "-h") == 0 ||
+	    strcmp(command, "--help") == 0)
 		return Help;
-	if (strcmp(command, "version") == 0 || strcmp(command, "-v") == 0)
+	if (strcmp(command, "version") == 0 || strcmp(command, "-v") == 0 ||
+	    strcmp(command, "--version") == 0)
 		return Version;
 	if (strcmp(command, "build") == 0) return Build;
 	if (strcmp(command, "clean") == 0) return Clean;
